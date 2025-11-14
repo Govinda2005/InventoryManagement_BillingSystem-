b56843af4b66891f02576486a6cfec6e8c929611
GUDISA GOVINDA <govindagudisa3427@gmail.com>
2025-11-14 12:13:50 +0530

    Refactor main.py to improve structure and imports

--- .venv/src/main.py
+++ .venv/src/main.py
@@ -1,41 +1,51 @@
-# src/main.py
-from src.admin import admin_login,view_products, search_product, update_product, delete_product, sales_report
-from src.customer import register_customer, customer_login, customer_menu
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import admin
+import customer
 
 def main_menu():
+    # asking whether he wants to login as admin or user
     while True:
-        print("\n Hello , Welcome to the Inventory  Management system through console")
-        login = input("\n Are you want to \n 1) login as admin \n 2) signup or login as User \n")
-        if login == '1':
-            if admin_login():
+        print("\n \n Hey Welcome to the  Inventory  Management system through console")
+        user_Logged= input("\n if you are a  \n 1) Admin  \n 2)  User \n")
+        # if user is an admin or customer
+        if user_Logged == '1':
+            # if user is a admin
+            if admin.loginAdmin():
                 while True:
-                    print("\n 1) Manage Products 2) Reports 3) Exit")
+                    print("\n 1) Manage Products  2) Reports 3) Exit ")
                     ch = input("Choose option: ")
                     if ch == '1':
-                        print("\n 1) View Products \n 2) Search Products \n 3) Update Products \n 4) Delete Products")
+                        print("\n 1) View Products  \n 2) Search Products \n 3) Update Products  \n 4) Delete Products")
                         sub = input("Choose: ")
                         if sub == '1':
-                            view_products()
+                            admin.displayProd()
                         elif sub == '2':
-                            search_product()
+                            admin.searchProd()
                         elif sub == '3':
-                            update_product()
+                            admin.updateProd()
                         elif sub == '4':
-                            delete_product()
+                            admin.deleteProd()
                     elif ch == '2':
-                        sales_report()
+                        admin.sales_report()
                     else:
                         break
-
-        elif login == '2':
-            print("1) Register 2) Login")
-            sub = input("Choose: ")
-            if sub == '1':
-                register_customer()
-            elif sub == '2':
-                cid = customer_login()
+    #for user login activities
+        elif user_Logged == '2':
+            #if user is a cutomer
+            print("1) Register a new user 2) Login as user")
+            #asking user input
+            usr_inp = input("your option : ")
+            # if user is new user
+            if usr_inp == '1':
+                customer.register_customer()
+                #already existing user
+            if usr_inp == '2':
+                cid = customer.customer_login()
                 if cid:
-                    customer_menu(cid)
+                    customer.customer_menu(cid)
 
 if __name__ == '__main__':
     main_menu()
